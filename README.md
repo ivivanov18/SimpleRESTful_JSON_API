f34d3745a9018d7128f62b996b1acd9e55b223bb
ivivanov18 <ivanov.ivan.v@gmail.com>
2019-01-06 16:35:10 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# First project of Node MasterClass
+
+## Description
+
+Simple RESTful JSON API that listens on the port **8080**. The API is developed in pure NodeJS and no frameworks were used.
+
+## Routes
+
+### /hello
+
+The API returns:
+
+- HTTP code: 200
+- JSON: `json{ msg: "Welcome to the Great Node.js" }`
+
+### Every other route
+
+Every other end point returns:
+
+- HTTP code: 404
+- JSON: `json{ msg: "Not found" }`
+
+## How to run
+
+- Clone the repo ``
--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const http = require("http");
 const url = require("url");
+const StringDecoder = require("string_decoder").StringDecoder;
 
+// CONFIG SERVER
 const port = 8080;
 const hostname = "localhost";
 
+// HANDLERS ROUTES
 const handlers = {};
 
 handlers.hello = (data, callback) => {
-  callback(200, { msg: "Hello World" });
+  callback(200, { msg: "Welcome to the Great Node.js" });
 };
 
 handlers.notFound = (data, callback) => {
@@ -23,9 +26,65 @@ const server = http.createServer((req, res) => {
   // true -> the query property will always be set to an object returned by the querystring // module's parse() method
   const parsedUrl = url.parse(req.url, true);
 
+  // get path from parsedURL
   const path = parsedUrl.pathname;
 
+  // apply regex to just leave the path
   const trimmedPath = path.replace(/^\/+|\/+$/g, "");
+
+  // get the headers
+  const headers = req.headers;
+
+  // get the method
+  const method = req.method.toLowerCase();
+
+  // get the query string as an object
+  const queryStringObject = parsedUrl.query;
+
+  // get the payload
+  const decoder = new StringDecoder("utf-8");
+  let buffer = "";
+  req.on("data", data => {
+    buffer += decoder.write(data);
+  });
+
+  req.on("end", () => {
+    buffer += decoder.end();
+
+    // get the handler according to path
+    const chosenHandler =
+      typeof router[trimmedPath] !== "undefined"
+        ? router[trimmedPath]
+        : handlers.notFound;
+
+    const data = {
+      trimmedPath,
+      queryStringObject,
+      method,
+      headers,
+      payload: buffer
+    };
+
+    // apply handler to send response
+    chosenHandler(data, (statusCode, payload) => {
+      const statusCodeToSend =
+        typeof statusCode === "number" ? statusCode : 200;
+
+      const payloadToSend =
+        typeof payload === "object" ? payload : { msg: "Just Welcome" };
+
+      const payloadToSendString = JSON.stringify(payloadToSend);
+
+      // response
+      res.setHeader("Content-Type", "application/json");
+      res.writeHead(statusCodeToSend);
+      res.end(payloadToSendString);
+    });
+  });
 });
 
-server.listen();
+server.listen(port, () => {
+  console.log(
+    `The server is listening on the port ${port} on the host ${hostname}`
+  );
+});
